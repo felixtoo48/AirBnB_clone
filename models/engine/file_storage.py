1294ae82430181871f720a075a98defa53adeeda
felixtoo48 <felixtoo48@gmail.com>
2022-11-28 17:27:07 +0300

    unique file storage instance of my application

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,4 +6,15 @@ class for the airbnb clone
 
 
 class FileStorage:
-
+    """File storage class for AirBnB 
+    clone project
+    Methods:
+        __file_path - string path to
+        json file
+        __objects - dictionary empty
+        but will store all objects
+        all(self)
+        new(self, obj)
+        save(self)
+        reload(self)
+    """
