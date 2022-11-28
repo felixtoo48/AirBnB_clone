2b1303246ea47f1f52122d56543a02877afe1bc3
felixtoo48 <felixtoo48@gmail.com>
2022-11-28 22:46:39 +0300

    quit and EOF functions

--- console.py
+++ console.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""
+Program that contains the entry point
+of the command interpreter
+"""
+
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
-    pass
+    def do_EOF(self, line):
+        """EOF command to quit the program"""
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
 
 
 if __name__ == '__main__':
