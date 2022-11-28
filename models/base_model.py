beab761e3fd62950308500280117e7f189de9962
felixtoo48 <felixtoo48@gmail.com>
2022-11-28 23:49:32 +0300

    indentation

--- models/base_model.py
+++ models/base_model.py
@@ -37,7 +37,7 @@ class BaseModel:
         """ initialize str
         returns class name, id and the dictionary"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
-                                       self.id, self.__dict__)
+                                     self.id, self.__dict__)
 
     def save(self):
         """ updates the public instance attributes
