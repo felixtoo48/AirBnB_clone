6c568c10fd737dde8bddc6ef8063f48a72cc5caa
felixtoo48 <felixtoo48@gmail.com>
2022-11-28 22:51:39 +0300

    Enter + empty file update

--- console.py
+++ console.py
@@ -9,6 +9,18 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
+
+    def emptyline(self):
+        """Called when an empty line is entered in response to the prompt.
+
+        If this method is not overridden, it repeats the last nonempty
+        command entered.
+
+        """
+        if self.lastcmd:
+            self.lastcmd = ""
+            return self.onecmd('\n')
+
     def do_EOF(self, line):
         """EOF command to quit the program"""
         return True
