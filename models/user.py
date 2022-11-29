8b5b845f658f6692f4312f6ded5ae1827564635a
felixtoo48 <felixtoo48@gmail.com>
2022-11-29 15:09:13 +0300

    state class

--- models/user.py
+++ models/user.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" User class module that inherits from
-BaseModel"""
+""" User class module that inherits from BaseModel"""
 
 from models.base_model import BaseModel
-import json
 
 
 class User(BaseModel):
-    """ initializes the user class
-    Sub class of base model
+    """A subclass of BaseModel class
+    Public class attribute:
+        name: (str)
+        password: (str)
+        first_name: (str)
+        last_name: (str)
     """
     email = ""
     password = ""
